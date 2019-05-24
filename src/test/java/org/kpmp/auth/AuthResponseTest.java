b3050afc2e088b1bc63b75aa51db241ae9077a40
Zach Wright <zwright@umich.edu>
2019-05-24 13:05:35 -0400

    KPMP-1024: Add some tests

--- src/test/java/org/kpmp/auth/AuthResponseTest.java
+++ src/test/java/org/kpmp/auth/AuthResponseTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import users.User;
 
 public class AuthResponseTest {
 
@@ -25,6 +26,13 @@ public class AuthResponseTest {
     public void testSetToken() throws Exception {
         authResponse.setToken("token");
         assertEquals("token", authResponse.getToken());
+    }
+
+    @Test
+    public void testSetUser() throws Exception {
+        User user = new User();
+        authResponse.setUser(user);
+        assertEquals(user, authResponse.getUser());
     }
 
 
