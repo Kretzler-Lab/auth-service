5c708a98d970d8a780a3269e7d615dd36d263638
Becky Reamy <rlreamy@umich.edu>
2019-10-08 13:23:08 -0400

    KPMP-1317: Remove warnings

--- src/main/java/org/kpmp/auth/UserPortalService.java
+++ src/main/java/org/kpmp/auth/UserPortalService.java
@@ -1,7 +1,5 @@
 package org.kpmp.auth;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpEntity;
@@ -14,33 +12,31 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class UserPortalService {
 
-    @Value("${userportal.url}")
-    private String userPortalURL;
+	@Value("${userportal.url}")
+	private String userPortalURL;
 
-    private final Environment env;
+	private final Environment env;
 
-    private final Logger log = LoggerFactory.getLogger(this.getClass());
+	public UserPortalService(Environment env) {
+		this.env = env;
+	}
 
-    public UserPortalService(Environment env) {
-        this.env = env;
-    }
+	public UserAuth getUserAuth(String shib_id) throws HttpClientErrorException {
+		return getUserAuth(env.getProperty("DEFAULT_CLIENT_ID"), shib_id);
+	}
 
-    public UserAuth getUserAuth(String shib_id) throws HttpClientErrorException {
-        return getUserAuth(env.getProperty("DEFAULT_CLIENT_ID"), shib_id);
-    }
+	public UserAuth getUserAuthWithClient(String clientId, String shib_id) throws HttpClientErrorException {
+		return getUserAuth(clientId, shib_id);
+	}
 
-    public UserAuth getUserAuthWithClient(String clientId, String shib_id) throws HttpClientErrorException {
-        return getUserAuth(clientId, shib_id);
-    }
-
-    private UserAuth getUserAuth(String clientId, String shib_id) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("X-API-TOKEN", clientId);
-        HttpEntity<?> entity = new HttpEntity<Object>("body", headers);
-        RestTemplate restTemplate = new RestTemplate();
-        UserAuth userAuth = restTemplate.exchange(userPortalURL + "/api/user/" + shib_id,
-                HttpMethod.GET, entity, UserAuth.class).getBody();
-        return userAuth;
-    }
+	private UserAuth getUserAuth(String clientId, String shib_id) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("X-API-TOKEN", clientId);
+		HttpEntity<?> entity = new HttpEntity<Object>("body", headers);
+		RestTemplate restTemplate = new RestTemplate();
+		UserAuth userAuth = restTemplate
+				.exchange(userPortalURL + "/api/user/" + shib_id, HttpMethod.GET, entity, UserAuth.class).getBody();
+		return userAuth;
+	}
 
 }
